ed05ba5bb12d2bf0ffc5bc9a12cc6239e396d2f2
maxifernandeaz <maximilianofernandezbaez@gmail.com>
2023-12-13 00:30:09 -0300

    se corrigio listado de producto js

--- js/listado.js
+++ js/listado.js
@@ -1,5 +1,6 @@
-const URL = "http://127.0.0.1:5000/";
-// const URL = "https://maxifb.pythonanywhere.com/";
+//const URL = "http://127.0.0.1:5000/";
+const URL = "https://maxifb.pythonanywhere.com/";
+
 
 
 fetch(URL + 'productos')
@@ -17,18 +18,15 @@ fetch(URL + 'productos')
 
         for (let producto of data) {
             let fila = document.createElement('tr');
-            fila.innerHTML = '<td>' + producto.codigo + '</td>' +
-                '<td>' + producto.descripcion + '</td>' +
-                '<td align="right">' + producto.cantidad + '</td>' +
-                '<td align="right">' + producto.precio + '</td>' +
-                // Corrige la URL de la imagen
-                '<td><img src="./static/css/imagenes/' + producto.imagen_url + '" alt="Imagen del producto" style="width: 100px;"></td>';
- +
-                '<td align="right">' + producto.proveedor + '</td>';
-                // '<td><img src=https://www.pythonanywhere.com/user/maxifb/files/home/maxifb/mysite/img/' + producto.imagen_url + ' alt="Imagen del producto" style="width: 100px;"></td>' + '<td align="right">' + producto.proveedor + '</td>';
-            
-            //Una vez que se crea la fila con el contenido del producto, se agrega la
-            //tabla utilizando el metodo appendchild del elemento tablaProductos.
+            fila.innerHTML = `<td>${producto.codigo}</td>` +
+                `<td>${producto.descripcion}</td>` +
+                `<td align="right">${producto.cantidad}</td>` +
+                `<td align="right">${producto.precio}</td>` +
+                `<td><img src="https://www.pythonanywhere.com/user/maxifb/files/home/maxifb/mysite/.static/css/imagenes/${producto.imagen_url}" alt="Imagen del producto" style="width: 100px;"></td>` +
+                `<td align="right">${producto.proveedor}</td>`;
+
+            // Una vez que se crea la fila con el contenido del producto, se agrega la
+            // tabla utilizando el método appendChild del elemento tablaProductos.
             tablaProductos.appendChild(fila);
         }
     })
@@ -36,3 +34,9 @@ fetch(URL + 'productos')
         alert('Error al obtener los productos.');
         console.error('Error:', error);
     });
+
+
+//'<td align="right">' + producto.proveedor + '</td>';
+
+// Corrige la URL de la imagen
+//'<td><img src="./static/css/imagenes/' + producto.imagen_url + '" alt="Imagen del producto" style="width: 100px;"></td>';
